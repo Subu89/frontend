c027fc2d060a97ea86081667b7d43807ca52077f
Subu89 <sudipmhnt@gmail.com>
2022-04-11 07:49:49 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import {ReactNavbar} from 'overlay-navbar';
+
+const Header = () => {
+    return <ReactNavbar
+    navColor1="white"
+    navColor2="hsl(219, 48%, 8%)"
+    />
+};
+
+export default Header;
